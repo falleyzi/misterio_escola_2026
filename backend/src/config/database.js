a0e65d7134cdd11c54ea5656280422e72213a587
Arthur <arthuraltkk@gmail.com>
2026-06-16 21:15:16 -0300

    fiz coisa dms

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,9 +2,16 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+
+console.log('PGHOST=', process.env.PGHOST);
+console.log('PGPORT=', process.env.PGPORT);
+console.log('PGUSER=', process.env.PGUSER);
+console.log('PGPASSWORD=', process.env.PGPASSWORD);
+console.log('PGDATABASE=', process.env.PGDATABASE);
+
 const pool = new Pool({
   host: process.env.PGHOST,
-  port: Number(process.env.PGPORT) || 5432,
+  port: Number(process.env.PGPORT) || 5436,
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
